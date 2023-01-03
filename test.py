07611f4b14aae0b4dc02dbd1011a5b50515d18ef
Orphila <120557052+Orphila@users.noreply.github.com>
2023-01-03 11:06:15 +0100

    Update test.py

--- test.py
+++ test.py
@@ -41,6 +41,8 @@ else :
 res = test_date - timedelta(days=diff)
 date_fin=str(res)[:10]
 
+if date_fin not in liste_jours_ouvrés():
+    date_fin="2022-12-29"
 
 if date_fin in liste_jours_ouvrés():
     #--------------------------création du dataset avec yahoo finance--------------
